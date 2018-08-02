ca1fd3fdb840d03c42f81ff7fe730fca026d98a1
Phap Duong <d2phap@gmail.com>
2018-08-02 22:57:05 +0800

    released v1.2

--- Nuget/build.bat
+++ Nuget/build.bat
@@ -1,3 +1,3 @@
-nuget.exe pack "FileWatcherEx.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version 1.1.0.0
+nuget.exe pack "FileWatcherEx.nuspec" -NoPackageAnalysis -verbosity detailed -o Build -Version 1.2.0.0
 
 pause
