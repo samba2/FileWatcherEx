00a9ea987b2df1b73ba6f037c6df8d5ae99e7547
Phap Dieu Duong <ddphap@gmail.com>
2021-12-10 13:46:05 +0800

    migrate to .net 6 + refactor + clean up

---
+++ v2/FileWatcherEx/FileWatcher.cs
@@ -0,0 +1,224 @@
+﻿/*---------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+
+namespace System.IO;
+
+
+internal class FileWatcher : IDisposable
+{
+    private string _watchPath = string.Empty;
+    private Action<FileChangedEvent>? _eventCallback = null;
+    private Dictionary<string, FileSystemWatcher> _fwDictionary = new();
+    private Action<ErrorEventArgs>? _onError = null;
+
+
+    /// <summary>
+    /// Create new instance of FileSystemWatcher
+    /// </summary>
+    /// <param name="path">Full folder path to watcher</param>
+    /// <param name="onEvent">onEvent callback</param>
+    /// <param name="onError">onError callback</param>
+    /// <returns></returns>
+    public FileSystemWatcher Create(string path, Action<FileChangedEvent> onEvent, Action<ErrorEventArgs> onError)
+    {
+        _watchPath = path;
+        _eventCallback = onEvent;
+        _onError = onError;
+
+        var watcher = new FileSystemWatcher
+        {
+            Path = _watchPath,
+            IncludeSubdirectories = true,
+            NotifyFilter = NotifyFilters.LastWrite
+                | NotifyFilters.FileName
+                | NotifyFilters.DirectoryName,
+        };
+
+        // Bind internal events to manipulate the possible symbolic links
+        watcher.Created += new(MakeWatcher_Created);
+        watcher.Deleted += new(MakeWatcher_Deleted);
+
+        watcher.Changed += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CHANGED));
+        watcher.Created += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CREATED));
+        watcher.Deleted += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.DELETED));
+        watcher.Renamed += new((object _, RenamedEventArgs e) => ProcessEvent(e));
+        watcher.Error += new ErrorEventHandler((object source, ErrorEventArgs e) => onError(e));
+
+        //changing this to a higher value can lead into issues when watching UNC drives
+        watcher.InternalBufferSize = 32768;
+        _fwDictionary.Add(path, watcher);
+
+        foreach (var dirInfo in new DirectoryInfo(path).GetDirectories())
+        {
+            var attrs = File.GetAttributes(dirInfo.FullName);
+
+            // TODO: consider skipping hidden/system folders? 
+            // See IG Issue #405 comment below
+            // https://github.com/d2phap/ImageGlass/issues/405
+            if (attrs.HasFlag(FileAttributes.Directory)
+                && attrs.HasFlag(FileAttributes.ReparsePoint))
+            {
+                try
+                {
+                    MakeWatcher(dirInfo.FullName);
+                }
+                catch
+                {
+                    // IG Issue #405: throws exception on Windows 10
+                    // for "c:\users\user\application data" folder and sub-folders.
+                }
+            }
+        }
+
+        return watcher;
+    }
+
+
+    /// <summary>
+    /// Process event for type = [CHANGED; DELETED; CREATED]
+    /// </summary>
+    /// <param name="e"></param>
+    /// <param name="changeType"></param>
+    private void ProcessEvent(FileSystemEventArgs e, ChangeType changeType)
+    {
+        _eventCallback?.Invoke(new()
+        {
+            ChangeType = changeType,
+            FullPath = e.FullPath,
+        });
+    }
+
+
+    /// <summary>
+    /// Process event for type = RENAMED
+    /// </summary>
+    /// <param name="e"></param>
+    private void ProcessEvent(RenamedEventArgs e)
+    {
+        _eventCallback?.Invoke(new()
+        {
+            ChangeType = ChangeType.RENAMED,
+            FullPath = e.FullPath,
+            OldFullPath = e.OldFullPath,
+        });
+    }
+
+
+    private void MakeWatcher(string path)
+    {
+        if (!_fwDictionary.ContainsKey(path))
+        {
+            var fileSystemWatcherRoot = new FileSystemWatcher
+            {
+                Path = path,
+                IncludeSubdirectories = true,
+                EnableRaisingEvents = true
+            };
+
+            // Bind internal events to manipulate the possible symbolic links
+            fileSystemWatcherRoot.Created += new(MakeWatcher_Created);
+            fileSystemWatcherRoot.Deleted += new(MakeWatcher_Deleted);
+
+            fileSystemWatcherRoot.Changed += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CHANGED));
+            fileSystemWatcherRoot.Created += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CREATED));
+            fileSystemWatcherRoot.Deleted += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.DELETED));
+            fileSystemWatcherRoot.Renamed += new((object _, RenamedEventArgs e) => ProcessEvent(e));
+            fileSystemWatcherRoot.Error += new((object _, ErrorEventArgs e) => _onError?.Invoke(e));
+
+            _fwDictionary.Add(path, fileSystemWatcherRoot);
+        }
+
+        foreach (var item in new DirectoryInfo(path).GetDirectories())
+        {
+            var attrs = File.GetAttributes(item.FullName);
+
+            // If is a directory and symbolic link
+            if (attrs.HasFlag(FileAttributes.Directory)
+                && attrs.HasFlag(FileAttributes.ReparsePoint))
+            {
+                if (!_fwDictionary.ContainsKey(item.FullName))
+                {
+                    var fswItem = new FileSystemWatcher
+                    {
+                        Path = item.FullName,
+                        IncludeSubdirectories = true,
+                        EnableRaisingEvents = true,
+                    };
+
+                    // Bind internal events to manipulate the possible symbolic links
+                    fswItem.Created += new(MakeWatcher_Created);
+                    fswItem.Deleted += new(MakeWatcher_Deleted);
+
+                    fswItem.Changed += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CHANGED));
+                    fswItem.Created += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CREATED));
+                    fswItem.Deleted += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.DELETED));
+                    fswItem.Renamed += new((object _, RenamedEventArgs e) => ProcessEvent(e));
+                    fswItem.Error += new((object _, ErrorEventArgs e) => _onError?.Invoke(e));
+
+                    _fwDictionary.Add(item.FullName, fswItem);
+                }
+
+                MakeWatcher(item.FullName);
+            }
+        }
+    }
+
+
+    private void MakeWatcher_Created(object sender, FileSystemEventArgs e)
+    {
+        try
+        {
+            var attrs = File.GetAttributes(e.FullPath);
+            if (attrs.HasFlag(FileAttributes.Directory)
+                && attrs.HasFlag(FileAttributes.ReparsePoint))
+            {
+                var watcherCreated = new FileSystemWatcher
+                {
+                    Path = e.FullPath,
+                    IncludeSubdirectories = true,
+                    EnableRaisingEvents = true
+                };
+
+                // Bind internal events to manipulate the possible symbolic links
+                watcherCreated.Created += new(MakeWatcher_Created);
+                watcherCreated.Deleted += new(MakeWatcher_Deleted);
+
+                watcherCreated.Changed += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CHANGED));
+                watcherCreated.Created += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.CREATED));
+                watcherCreated.Deleted += new((object _, FileSystemEventArgs e) => ProcessEvent(e, ChangeType.DELETED));
+                watcherCreated.Renamed += new((object _, RenamedEventArgs e) => ProcessEvent(e));
+                watcherCreated.Error += new((object _, ErrorEventArgs e) => _onError?.Invoke(e));
+
+                _fwDictionary.Add(e.FullPath, watcherCreated);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("<<ERROR>>: " + ex.Message);
+        }
+    }
+
+
+    private void MakeWatcher_Deleted(object sender, FileSystemEventArgs e)
+    {
+        // If object removed, then I will dispose and remove them from dictionary
+        if (_fwDictionary.ContainsKey(e.FullPath))
+        {
+            _fwDictionary[e.FullPath].Dispose();
+            _fwDictionary.Remove(e.FullPath);
+        }
+    }
+
+
+    /// <summary>
+    /// Dispose the instance
+    /// </summary>
+    public void Dispose()
+    {
+        foreach (var item in _fwDictionary)
+        {
+            item.Value.Dispose();
+        }
+    }
+}
---
+++ v2/FileWatcherEx/FileWatcherEx.cs
@@ -0,0 +1,266 @@
+﻿
+using System.Collections.Concurrent;
+using System.ComponentModel;
+
+namespace System.IO;
+
+public class FileWatcherEx : IDisposable
+{
+
+    #region Private Properties
+
+    private Thread _thread;
+    private EventProcessor _processor;
+    private BlockingCollection<FileChangedEvent> _fileEventQueue = new();
+
+    private FileWatcher _watcher = new();
+    private FileSystemWatcher _fsw = new();
+
+    #endregion
+
+
+
+    #region Public Properties
+
+    /// <summary>
+    /// Folder path to watch
+    /// </summary>
+    public string FolderPath { get; set; } = "";
+
+
+    /// <summary>
+    /// Filter string used for determining what files are monitored in a directory
+    /// </summary>
+    public string Filter { get; set; } = "*.*";
+
+
+    /// <summary>
+    /// Gets, sets the type of changes to watch for
+    /// </summary>
+    public NotifyFilters NotifyFilter { get; set; } = NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+
+    /// <summary>
+    /// Gets or sets a value indicating whether subdirectories within the specified path should be monitored.
+    /// </summary>
+    public bool IncludeSubdirectories { get; set; } = false;
+
+
+    /// <summary>
+    /// Gets or sets the object used to marshal the event handler calls issued as a result of a directory change.
+    /// </summary>
+    public ISynchronizeInvoke SynchronizingObject { get; set; }
+
+    #endregion
+
+
+
+    #region Public Events
+    public delegate void DelegateOnChanged(object sender, FileChangedEvent e);
+    public event DelegateOnChanged OnChanged;
+
+    public delegate void DelegateOnDeleted(object sender, FileChangedEvent e);
+    public event DelegateOnDeleted OnDeleted;
+
+    public delegate void DelegateOnCreated(object sender, FileChangedEvent e);
+    public event DelegateOnCreated OnCreated;
+
+    public delegate void DelegateOnRenamed(object sender, FileChangedEvent e);
+    public event DelegateOnRenamed OnRenamed;
+
+    public delegate void DelegateOnError(object sender, ErrorEventArgs e);
+    public event DelegateOnError OnError;
+    #endregion
+
+
+
+    /// <summary>
+    /// Initialize new instance of FileWatcherEx
+    /// </summary>
+    /// <param name="folder"></param>
+    public FileWatcherEx(string folder = "")
+    {
+        FolderPath = folder;
+    }
+
+
+    /// <summary>
+    /// Start watching files
+    /// </summary>
+    public void Start()
+    {
+        if (!Directory.Exists(FolderPath)) return;
+
+
+        _processor = new EventProcessor((e) =>
+        {
+            switch (e.ChangeType)
+            {
+                case ChangeType.CHANGED:
+
+                    InvokeChangedEvent(SynchronizingObject, e);
+
+                    void InvokeChangedEvent(object sender, FileChangedEvent fileEvent)
+                    {
+                        if (SynchronizingObject != null && SynchronizingObject.InvokeRequired)
+                        {
+                            SynchronizingObject.Invoke(new Action<object, FileChangedEvent>(InvokeChangedEvent), new object[] { SynchronizingObject, e });
+                        }
+                        else
+                        {
+                            OnChanged?.Invoke(SynchronizingObject, e);
+                        }
+                    }
+
+
+                    break;
+
+                case ChangeType.CREATED:
+
+                    InvokeCreatedEvent(SynchronizingObject, e);
+
+                    void InvokeCreatedEvent(object sender, FileChangedEvent fileEvent)
+                    {
+                        if (SynchronizingObject != null && SynchronizingObject.InvokeRequired)
+                        {
+                            SynchronizingObject.Invoke(new Action<object, FileChangedEvent>(InvokeCreatedEvent), new object[] { SynchronizingObject, e });
+                        }
+                        else
+                        {
+                            OnCreated?.Invoke(SynchronizingObject, e);
+                        }
+                    }
+
+
+                    break;
+
+                case ChangeType.DELETED:
+
+                    InvokeDeletedEvent(SynchronizingObject, e);
+
+                    void InvokeDeletedEvent(object sender, FileChangedEvent fileEvent)
+                    {
+                        if (SynchronizingObject != null && SynchronizingObject.InvokeRequired)
+                        {
+                            SynchronizingObject.Invoke(new Action<object, FileChangedEvent>(InvokeDeletedEvent), new object[] { SynchronizingObject, e });
+                        }
+                        else
+                        {
+                            OnDeleted?.Invoke(SynchronizingObject, e);
+                        }
+                    }
+
+
+                    break;
+
+                case ChangeType.RENAMED:
+
+                    InvokeRenamedEvent(SynchronizingObject, e);
+
+                    void InvokeRenamedEvent(object sender, FileChangedEvent fileEvent)
+                    {
+                        if (SynchronizingObject != null && SynchronizingObject.InvokeRequired)
+                        {
+                            SynchronizingObject.Invoke(new Action<object, FileChangedEvent>(InvokeRenamedEvent), new object[] { SynchronizingObject, e });
+                        }
+                        else
+                        {
+                            OnRenamed?.Invoke(SynchronizingObject, e);
+                        }
+                    }
+
+
+                    break;
+
+                default:
+                    break;
+            }
+
+
+        }, (log) =>
+        {
+            Console.WriteLine(string.Format("{0} | {1}", Enum.GetName(typeof(ChangeType), ChangeType.LOG), log));
+        });
+
+
+        _thread = new Thread(() =>
+        {
+            while (true)
+            {
+                var e = _fileEventQueue.Take();
+                _processor.ProcessEvent(e);
+            }
+        })
+        {
+            // this ensures the thread does not block the process from terminating!
+            IsBackground = true
+        };
+
+        _thread.Start();
+
+
+        // Log each event in our special format to output queue
+        void onEvent(FileChangedEvent e)
+        {
+            _fileEventQueue.Add(e);
+        }
+
+
+        // OnError
+        void onError(ErrorEventArgs e)
+        {
+            if (e != null)
+            {
+                OnError?.Invoke(this, e);
+            }
+        }
+
+
+        // Start watcher
+        _watcher = new FileWatcher();
+
+        _fsw = _watcher.Create(FolderPath, onEvent, onError);
+        _fsw.Filter = Filter;
+        _fsw.NotifyFilter = NotifyFilter;
+        _fsw.IncludeSubdirectories = IncludeSubdirectories;
+        _fsw.SynchronizingObject = SynchronizingObject;
+
+        // Start watching
+        _fsw.EnableRaisingEvents = true;
+    }
+
+
+    /// <summary>
+    /// Stop watching files
+    /// </summary>
+    public void Stop()
+    {
+        if (_fsw != null)
+        {
+            _fsw.EnableRaisingEvents = false;
+        }
+
+        if (_watcher != null)
+        {
+            _watcher.Dispose();
+        }
+
+        if (_thread != null)
+        {
+            _thread.Abort();
+        }
+    }
+
+
+    /// <summary>
+    /// Dispose the FileWatcherEx instance
+    /// </summary>
+    public void Dispose()
+    {
+        if (_fsw != null)
+        {
+            _fsw.Dispose();
+        }
+    }
+
+}
