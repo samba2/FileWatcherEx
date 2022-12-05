b4669fd04574a59676ecbe77cb81140e6830c928
Maik Toepfer <maik@toepfer.nu>
2022-12-05 07:48:35 +0100

    simple real file system test

---
+++ Source/FileWatcherExTests/FileWatcherExIntegrationTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Concurrent;
+using FileWatcherEx;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace FileWatcherExTests;
+
+public class FileWatcherExIntegrationTest
+{
+    [Fact]
+    public void SimpleRealFileSystemTest()
+    {
+        ConcurrentQueue<FileChangedEvent> events = new();
+        var fw = new FileSystemWatcherEx(@"c:\temp\fwtest\");
+
+        fw.OnCreated += (_, ev) => events.Enqueue(ev);
+        fw.OnDeleted += (_, ev) => events.Enqueue(ev);
+        fw.OnChanged += (_, ev) => events.Enqueue(ev);
+        fw.OnRenamed += (_, ev) => events.Enqueue(ev);
+        fw.OnRenamed += (_, ev) => events.Enqueue(ev);
+
+        const string testFile = @"c:\temp\fwtest\b.txt";
+        if (File.Exists(testFile))
+        {
+            File.Delete(testFile);
+        }
+
+        fw.Start();
+        File.Create(testFile);
+        Thread.Sleep(250);
+        fw.Stop();
+
+        Assert.Single(events);
+        var ev = events.First();
+        Assert.Equal(ChangeType.CREATED, ev.ChangeType);
+        Assert.Equal(@"c:\temp\fwtest\b.txt", ev.FullPath);
+        Assert.Equal("", ev.OldFullPath);
+    }
+}
